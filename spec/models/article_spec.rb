dff1e7b4694c891103abcec804792ee0841e309d
Shannarra <petarangelov15@gmail.com>
2023-10-10 14:51:28 +0300

    Added some more specs

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,11 +3,32 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   let(:first_article) { create(:article) }
 
-
-  describe 'can be created' do
+  describe 'with valid attributes' do
     it 'creates a new article' do
       expect(first_article).to be_valid
       expect(Article.count).to eq 1
+    end
+  end
+
+  describe 'with invalid attributes' do
+    context 'when empty' do
+      let(:empty_article) { create(:article, title: '', content: '') }
+    
+      it 'does not create a new article' do
+        expect {
+          empty_article
+        }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context 'when violating min-max boundaries' do
+      let(:invalid_article) { create(:article, title: 'a'*101, content: 'a'*100_001) }
+    
+      it 'does not create a new article' do
+        expect {
+          invalid_article
+        }.to raise_error(ActiveRecord::RecordInvalid)
+      end
     end
   end
 end
