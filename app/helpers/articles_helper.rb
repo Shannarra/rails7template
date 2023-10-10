99201ce4ed35b7d96192bd1e1464f3260ab8af1f
Shannarra <petarangelov15@gmail.com>
2023-10-10 13:44:31 +0300

    Fixed potential bug in articles helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,5 +1,5 @@
 module ArticlesHelper
   def localized_creation_time(article)
-    article.created_at.in_time_zone(ENV.fetch("TIMEZONE"))
+    article.created_at.in_time_zone(ENV.try("TIMEZONE", "UTC"))
   end
 end
