fbfa1b68187ffebc52e1f7a819bfc9a72a04d086
Shannarra <petarangelov15@gmail.com>
2023-10-10 13:59:26 +0300

    Rubocop fix

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
   def application_timezone
     ENV.try(:[], "TIMEZONE") || "UTC"
   end
-  
+
   def localized_creation_time(article)
     article.created_at.in_time_zone(application_timezone)
   end
