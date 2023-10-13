b76f96ddb4b08b3efc65a677585461916e9f8471
Shannarra <petarangelov15@gmail.com>
2023-10-13 13:23:30 +0300

    Added html generation and migrations

--- startup.sh
+++ startup.sh
@@ -8,6 +8,11 @@ while [ $# -gt 0 ]; do
       USES_DEVISE=1
       shift # past argument
 
+      if [ -z $1 ]; then
+          echo "ERROR: If you want to create a devise, please provide a name! Use --help for more info."
+          exit 1
+      fi
+      
       DEVISE_NAME=$1
       shift # past value
       ;;
@@ -28,35 +33,63 @@ cp .env.example .env
 
 docker compose build
 
-echo "[STARTUP] Setting up Devise gem..."
+if [ $USES_DEVISE -eq 1 ]; then
 
-sed -i "65 i \\
+    echo "[STARTUP] Setting up Devise gem..."
+
+    sed -i "65 i \\
 # Useful to bootstrap authentication. https://stackoverflow.com/a/42190260/11542917 \\
 " Gemfile
-sed -i "66 i gem 'devise'" Gemfile
+    sed -i "66 i gem 'devise'" Gemfile
 
-# install gem
-docker compose run --rm web bundle
+    # install gem
+    docker compose run --rm web bundle
 
-# do the setup
-docker compose run --rm web rails g devise:install
+    # do the setup
+    docker compose run --rm web rails g devise:install
 
-# enable :turbo_stream
-sed -i "266 i \\
+    # enable :turbo_stream
+    sed -i "266 i \\
   config.navigational_formats = ['*/*', :html, :turbo_stream] \\
 " config/initializers/devise.rb
 
-echo "[STARTUP] Creating devise $DEVISE_NAME...."
+    echo "[STARTUP] Creating devise $DEVISE_NAME...."
 
-# https://www.digitalocean.com/community/tutorials/how-to-set-up-user-authentication-with-devise-in-a-rails-7-application
-docker compose run --rm web rails g devise $DEVISE_NAME
+    # https://www.digitalocean.com/community/tutorials/how-to-set-up-user-authentication-with-devise-in-a-rails-7-application
+    docker compose run --rm web rails g devise $DEVISE_NAME
 
-if [ $? -eq 0 ]; then
-  echo "[STARTUP] Devise $DEVISE_NAME successfully created!\nMigrating database..."
-  docker compose run --rm web rails db:migrate
+    if [ $? -eq 0 ]; then
+        echo "[STARTUP] Setting up database"
+        docker compose run --rm web bin/rails db:create
+        docker compose run --rm web rails db:migrate
+
+        echo "[STARTUP] Devise $DEVISE_NAME successfully created!"
+    else
+        echo "[STARTUP] Could not create device $DEVISE_NAME :("
+        exit 1
+    fi
+    
+    sed -i "42 i \\
+            <device-buttons>
+" app/views/layouts/application.html.erb
+
+#     sed -i "/<device-buttons>/ {
+#     s/<device-buttons>//g
+#     a\<form class=''>
+#               <a class='btn btn-outline-primary mr-2' href='#'>Login</a>
+#               <a class='btn btn-outline-success' href='#'>Become member</a>
+#             </form>
+# }" app/views/layouts/application.html.erb
+
+    sed -i "/<device-buttons>/ {
+    s/<device-buttons>//g
+    a\            <form class=''> \\
+              <a class='btn btn-outline-primary mr-2' href='#'>Login</a> \\
+              <a class='btn btn-outline-success' href='#'>Become member</a> \\
+            </form>
+}" app/views/layouts/application.html.erb 
+
 else
-  echo "[STARTUP] Could not create device $DEVISE_NAME :("
+    echo "[STARTUP] Setting up database"
+    docker compose run --rm web bin/rails db:setup
 fi
-
-echo "[STARTUP] Setting up database"
-docker compose run --rm web bin/rails db:setup
