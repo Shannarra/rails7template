d2bdbacac0dc4e5cd4304bc6495d4a404bbac13a
Shannarra <petarangelov15@gmail.com>
2024-07-31 14:43:03 +0300

    Keep .env helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def application_timezone
+    ENV.try(:fetch, 'TIMEZONE', 'UTC')
+  end
+
+  def localized_creation_time(object)
+    object.created_at.in_time_zone(application_timezone)
+  end
 end
